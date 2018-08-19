f4257c64330776c34be54ead0831f6a28e708ce3
goksankadir <iamgoksan@gmail.com>
2018-08-19 16:10:13 +0100

    Added URL for posting threads

--- denouement/urls.py
+++ denouement/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('forums/categories/<int:id>/<title>', views.view_forum_category, name='forum_category_titled'),
     path('forums/thread/<int:id>', views.view_forum_thread_untitled, name='forum_thread_untitled'),
     path('forums/thread/<int:id>/<title>', views.view_forum_thread, name='forum_thread_titled'),
+    path('forums/post/<category_id>', views.post_thread, name='post_thread'),
     path('account', views.view_account, name='view_account'),
     path('account/signup', views.sign_up, name='sign_up'),
     path('account/signin', views.sign_in, name='sign_in'),

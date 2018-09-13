4d07e7d85b295f1211ec94bb236fcec177cf02af
goksankadir <iamgoksan@gmail.com>
2018-09-13 11:36:27 +0100

    Added test to ensure duplicate usernames are denied regardless of case sensitivity

--- denouement/tests.py
+++ denouement/tests.py
@@ -9,15 +9,32 @@ class SignUpViewTests(TestCase):
 
     # Ensure the backend sees duplicate information and prompts user to change desired login credentials
     def test_denies_duplicate_username(self):
-        sign_up_response = self.client.post('/account/signup/', {'username': 'test', 'password': 'abcd123!', 'email': 'test@test.com'})
+        sign_up_response = self.client.post('/account/signup/', 
+            {'username': 'test', 'password': 'abcd123!', 'email': 'test@test.com'})
+        
         duplicate_username_sign_up_response = self.client.post('/account/signup/', 
             {'username': 'test', 'password': 'test101@!', 'email': 'test123@test.com'})
+        
         self.assertTemplateUsed(duplicate_username_sign_up_response, 'denouement/sign_up.html')
 
+    # Ensure the backend denies identical usernames regardless of letter case and reprompts entry of desired credentials
+    def test_denies_duplicate_username_case_insensitive(self):
+        sign_up_response = self.client.post('/account/signup/', 
+            {'username': 'test', 'password': 'abcd123!', 'email': 'test@test.com'})
+
+        duplicate_username_case_insensitive_sign_up_response = self.client.post('/account/signup/', 
+            {'username': 'tEsT', 'password': 'test101@!', 'email': 'test123@test.com'})
+
+        self.assertTemplateUsed(duplicate_username_case_insensitive_sign_up_response, 'denouement/sign_up.html')
+    
+    # Ensure the backend sees duplicate information and prompts user to change desired login credentials
     def test_denies_duplicate_email(self):
-        sign_up_response = self.client.post('/account/signup/', {'username': 'test', 'password': 'abcd123!', 'email': 'abc@test.com'})
+        sign_up_response = self.client.post('/account/signup/', 
+            {'username': 'test', 'password': 'abcd123!', 'email': 'abc@test.com'})
+
         duplicate_email_sign_up_response = self.client.post('/account/signup/', 
-            {'username': 'test', 'password': 'test101@!', 'email': 'abc@test.com'})
+            {'username': 'test123', 'password': 'test101@!', 'email': 'abc@test.com'})
+
         self.assertTemplateUsed(duplicate_email_sign_up_response, 'denouement/sign_up.html')
 
 

100b1a42148aab750206e4793d5ef376c9a96b6b
goksankadir <iamgoksan@gmail.com>
2018-09-17 13:04:57 +0100

    Edited test post URL's to reflect urls.py changes

--- denouement/tests.py
+++ denouement/tests.py
@@ -14,13 +14,13 @@ class SignUpViewTests(TestCase):
         sign_up_response = self.client.post('/account/signup/', 
             {'username': 'test', 'password': 'abcd123!', 'email': 'test@test.com'})
         
-        duplicate_username_sign_up_response = self.client.post('/account/signup/', 
+        duplicate_username_sign_up_response = self.client.post('forums/account/signup/', 
             {'username': 'test', 'password': 'test101@!', 'email': 'test123@test.com'})
         
         self.assertRaises(ObjectDoesNotExist, User.objects.get, email='test123@test.com')
 
     def test_denies_duplicate_username_case_insensitive(self):
-        sign_up_response = self.client.post('/account/signup/', 
+        sign_up_response = self.client.post('/forums/account/signup/', 
             {'username': 'test', 'password': 'abcd123!', 'email': 'test@test.com'})
 
         duplicate_username_case_insensitive_sign_up_response = self.client.post('/account/signup/', 
@@ -29,10 +29,10 @@ class SignUpViewTests(TestCase):
         self.assertRaises(ObjectDoesNotExist, User.objects.get, email='test123@test.com')
 
     def test_denies_duplicate_email(self):
-        sign_up_response = self.client.post('/account/signup/', 
+        sign_up_response = self.client.post('/forums/account/signup/', 
             {'username': 'test', 'password': 'abcd123!', 'email': 'abc@test.com'})
 
-        duplicate_email_sign_up_response = self.client.post('/account/signup/', 
+        duplicate_email_sign_up_response = self.client.post('/forums/account/signup/', 
             {'username': 'test123', 'password': 'test101@!', 'email': 'abc@test.com'})
     
         self.assertRaises(ObjectDoesNotExist, User.objects.get, username='test123')
@@ -44,9 +44,11 @@ class SignInViewTests(TestCase):
         user = User.objects.create_user(username='test', password='test123!@')
 
     def test_username_case_insensitive(self):
-        sign_in_response = self.client.post('/account/signin/',
+        sign_in_response = self.client.post('/forums/account/signin/',
             {'username': 'TeSt', 'password': 'test123!@'}, follow=True)
 
         self.assertTrue(sign_in_response.context['user'].is_authenticated)
+
+
 
 
